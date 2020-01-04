058b8c8fab77712dfad2a271971b0dcfed2352c8
Collins Muriuki <murerwacollins@gmail.com>
2020-01-04 11:00:55 +0300

    Adding background images to the menu items and dynamically setting height.

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title }) => (
-    <div className='menu-item'>
+const MenuItem = ({ title, imageUrl, size }) => (
+    <div style={{
+        backgroundImage: `url(${imageUrl})`
+    }} className={`${size} menu-item`}>
         <div className='content'>
             <div className='title'>{title}</div>
             <span className='subtitle'>SHOP NOW</span>
