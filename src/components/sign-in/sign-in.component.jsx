dc5ff4413dc08c4f02450c3b2ac9c4372f7a509d
Collins Muriuki <49271447+collinsmuriuki@users.noreply.github.com>
2020-01-06 22:15:23 +0300

    Styling the sign-in page

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import './sign-in.styles.scss';
 
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 
 class SignIn extends React.Component {
@@ -30,8 +31,8 @@ class SignIn extends React.Component {
     render() {
         return (
             <div className='sign-in'>
-                <h2>I already have an account</h2>
-                <span>Sign in with your email and password</span>
+                <h2 className='title'>I already have an account</h2>
+                <span className='title'>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
                     <FormInput
@@ -50,7 +51,7 @@ class SignIn extends React.Component {
                     handleChange={this.handleChange}
                     required />
 
-                    <input type='submit' value='Submit' />
+                    <CustomButton type='submit' value='Submit'> Sign in </CustomButton>
                 </form>
             </div>
         )
