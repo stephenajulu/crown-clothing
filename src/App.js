e4746d0d5ba174a3b6bc4de9689604b649b7c811
Collins Muriuki <49271447+collinsmuriuki@users.noreply.github.com>
2020-01-06 21:14:17 +0300

    Create initial auth page and sign-in components

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import AuthPage from './pages/auth/auth.component';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={AuthPage} />
       </Switch>
     </div>
   );
