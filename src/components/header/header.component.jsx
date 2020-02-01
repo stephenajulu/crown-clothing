afb3aff94daed410a533122a876d88cceae2c6c2
Collins Muriuki <murerwacollins@gmail.com>
2020-02-01 12:31:05 +0300

    implement redux to the cart -icon, cart-dropdown  and header components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -11,7 +11,7 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-contanier' to='/'>
       <Logo className='logo' />
@@ -38,12 +38,18 @@ const Header = ({ currentUser }) => (
 
       <CartIcon />
     </div>
-    <CartDropdown />
+
+    {
+      hidden ? null : 
+      <CartDropdown />
+    }
+    
   </div>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
 });
 
 export default connect(mapStateToProps)(Header);
