f305fc3e3d0d4f8e076d7bb2e24ee03e7d27f5cf
Collins Muriuki <murerwacollins@gmail.com>
2020-02-02 10:49:41 +0300

    Add CustomButton to collection preview component and its accompanying styling

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import CustomButton from '../custom-button/custom-button.component';
+
 import './collection-item.styles.scss';
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
@@ -12,9 +14,10 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       />
     
     <div className='collection-footer'>
-    <span className='name'>{name}</span>
-    <span className='price'>{price}</span>
+      <span className='name'>{name}</span>
+      <span className='price'>{price}</span>
     </div>
+    <CustomButton inverted>Add to cart</CustomButton>
   </div>
 );
 
