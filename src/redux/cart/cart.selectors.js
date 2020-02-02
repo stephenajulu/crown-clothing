f55324e0eea489438ce3a3e9265f54d1e9541afe
Collins Muriuki <murerwacollins@gmail.com>
2020-02-02 23:26:00 +0300

    Adding useful comments to cart selectors

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -1,10 +1,11 @@
 import { createSelector } from 'reselect';
 
+// inputselector; a function which gets the whole statesand returns a piece of it 
 const selectCart = state => state.cart;
 
 export const selectCartItems = createSelector(
   [selectCart],
-  (cart) => cart.cartItems
+  cart => cart.cartItems
 );
 
 export const selectCartItemsCount = createSelector(
