52fddc377a0ca92a45ea28db7d9ba2f2daffbebd
Collins Muriuki <murerwacollins@gmail.com>
2020-02-02 11:42:25 +0300

    Linting

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,30 +1,32 @@
-import { cartActionTypes } from './cart.types';
+import {
+  cartActionTypes
+} from './cart.types';
 
 const INITIAL_STATE = {
-    hidden: true,
-    cartItems: []
+  hidden: true,
+  cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
-    switch (action.type) {
-        case cartActionTypes.TOGGLE_CART_HIDDEN:
-            return {
-                ...state,
-                hidden: !state.hidden
-            };
-        
-        case cartActionTypes.ADD_ITEM:
-            return {
-                ...state,
-                cartItems: [
-                    ...state.cartItems,
-                    action.payload
-                ]
-            };
-        
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case cartActionTypes.TOGGLE_CART_HIDDEN:
+      return {
+        ...state,
+        hidden: !state.hidden
+      };
+
+    case cartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: [
+          ...state.cartItems,
+          action.payload
+        ]
+      };
+
+    default:
+      return state;
+  }
 }
 
 export default cartReducer;
