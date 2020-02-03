fb48c708dee389412645bdba1837bc228e28fb41
Collins Muriuki <murerwacollins@gmail.com>
2020-02-03 12:15:58 +0300

    Implement shorthand version of dispatch to toggle hide the cart dropdown component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -7,10 +7,11 @@ import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 
 import { selectCartItems } from '../../redux/cart/cart.selectors'
+import { toggleCartHidden } from '../../redux/cart/cart.action';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
       {
@@ -25,7 +26,13 @@ const CartDropdown = ({ cartItems, history }) => (
         )
       }
     </div>
-    <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+
+    <CustomButton onClick={() => {
+      history.push('/checkout');
+      dispatch(toggleCartHidden())
+      }}>
+        GO TO CHECKOUT
+    </CustomButton>
   </div>
 );
 
