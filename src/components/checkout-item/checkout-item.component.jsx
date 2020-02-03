da7d7b572021ca97232695e84f2f20a9f2ae2b2c
Collins Muriuki <murerwacollins@gmail.com>
2020-02-03 15:54:13 +0300

    [redux] Include removeItem action + dispatch from the checkout-item component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux'
 
-import { clearItemFromCart } from '../../redux/cart/cart.action';
+import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.action';
 
 import './checkout-item.styles.scss';
 
@@ -13,7 +13,13 @@ const CheckoutItem = ({ cartItem, dispatch }) => {
         <img src={imageUrl} alt='item' />
       </div>
       <span className='name'>{name}</span>
-      <span className='quantity'>{quantity}</span>
+      <span className='quantity'>
+        <div className='arrow' onClick={() => 
+          dispatch(removeItem(cartItem))}>&#10094;</div>
+          <span className='value'>{quantity}</span>
+        <div className='arrow' onClick={() => 
+          dispatch(addItem(cartItem))}>&#10095;</div>
+      </span>
       <span className='price'>{price}</span>
       <div className='remove-button' onClick={() =>
         dispatch(clearItemFromCart(cartItem))}>&#10005;</div>
