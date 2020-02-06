172035ced1aa2aab05e676c9142fa25f66821689
Collins Muriuki <murerwacollins@gmail.com>
2020-02-06 14:45:10 +0300

    Iterate through collection items to render CollectionItem comonent; also include styling for CollectionPage component

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -7,16 +7,22 @@ import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
-const CollectionPage = ({ match, collection }) => (
-  <div className='collection'>
-    <h2>{match.params.collectionId.toUpperCase()}</h2>
-    {
-      collection.items.map(item => (
-        <CollectionItem key={item.id} item={item} />
-      ))
-    }
-  </div>
-);
+const CollectionPage = ({ match, collection }) => {
+  const { items, title } = collection;
+
+  return (
+    <div className='collection-page'>
+      <h2 className='title'>{title.toUpperCase()}</h2>
+      <div className='items'>
+        {
+          items.map(item => (
+            <CollectionItem key={item.id} item={item} />
+          ))
+        }
+      </div>
+    </div>
+  )
+};
 
 const mapStateToProps = (state, ownProps) => ({
   collection: selectCollection(ownProps.match.params.collectionId)(state) // state will need to be passed in the selector; this selector needs a part of the state depending on the url
