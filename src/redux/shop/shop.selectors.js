62840616b6ad4c139cd51194e5fd9cd46ab13e2f
Collins Muriuki <murerwacollins@gmail.com>
2020-02-06 14:45:18 +0300

    Linting

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,25 +1,25 @@
 import { createSelector } from 'reselect';
 
 const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5
 };
 
 const selectShop = state => state.shop;
 
 export const selectShopCollectionItems = createSelector(
-    [selectShop],
-    items => items.shopItems
+  [selectShop],
+  items => items.shopItems
 );
 
 export const selectCollection = collectionUrlParam => (
-    createSelector(
-        [selectShopCollectionItems],
-        collections => collections.find(
-            collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-        )
+  createSelector(
+    [selectShopCollectionItems],
+    collections => collections.find(
+      collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
     )
+  )
 );
