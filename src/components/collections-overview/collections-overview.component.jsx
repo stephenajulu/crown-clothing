8da87de182183ec85f157080d7a1c54b0df5d3ab
Collins Muriuki <murerwacollins@gmail.com>
2020-02-06 21:27:01 +0300

    [redux] Add and implement selectCollectionsForPreview into CollectionsOverview component

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -4,22 +4,22 @@ import { createStructuredSelector } from 'reselect';
 
 import './collections-overview.styles.scss';
 
-import { selectShopCollectionItems } from '../../redux/shop/shop.selectors';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
 import CollectionPreview from '../collection-preview/collection-preview.component';
 
 const CollectionsOverview = ({ collections }) => (
     <div className='collections-overview'>
-            {
+    {      
       collections.map(({ id, ...otherCollectionProps }) => (
-        <CollectionPreview key={id}  {...otherCollectionProps} />
+        <CollectionPreview key={id} {...otherCollectionProps} />
       ))
     }
     </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectShopCollectionItems
+    collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
