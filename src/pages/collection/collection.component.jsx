fa644d6be62fcad54100876254c8b5484750260b
Collins Muriuki <murerwacollins@gmail.com>
2020-02-06 12:01:14 +0300

    [redux] Implement collectionId selector to the CollectionPage component

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import './collection.styles.scss';
 
+import { selectCollection } from '../../redux/shop/shop.selectors';
+
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
-const CollectionPage = ({ match }) => (
+const CollectionPage = ({ match, collection }) => (
   <div className='category'>
-    <h2>COLLECTION {match.params.collectionId}</h2>
+    <h2>{match.params.collectionId.toUpperCase()}</h2>
+    {
+      console.log(collection)
+    }
   </div>
 );
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state) // state will need to be passed in the selector; this selector needs a part of the state depending on the url
+});
+
+export default connect(mapStateToProps)(CollectionPage);
