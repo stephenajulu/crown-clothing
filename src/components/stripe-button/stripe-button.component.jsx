c80d862c8ece59bd859952750113fd0afa008958
Collins Muriuki <murerwacollins@gmail.com>
2020-02-07 18:52:59 +0300

    Create StripeCheckoutButton component implementing react-stripe-checkout packege

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+import './stripe-button.styles.scss';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_DX93HOnXHRWGxo1tMxHlecNj00MZV1t0MS';
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful');
+  }
+
+  return (
+    <StripeCheckout
+    lable='Pay Now'
+    name='Crown Clothing'
+    billingAddress
+    shippingAddress
+    image='https://svgshare.com/i/CUz.svg'
+    description={`Your total is $${price}`}
+    amount={priceForStripe}
+    panelLabel='Pay Now'
+    token={onToken}
+    publishableKey={publishableKey}
+    />
+  )
+}
+
+export default StripeCheckoutButton;
