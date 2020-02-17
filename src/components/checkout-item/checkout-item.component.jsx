dd331bbb3b5019eff7ed4f48bcddf81677a78d5f
Collins Muriuki <murerwacollins@gmail.com>
2020-02-17 16:38:54 +0300

    Convert checkout-item component styles to styled components

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -3,27 +3,29 @@ import { connect } from 'react-redux'
 
 import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.action';
 
-import './checkout-item.styles.scss';
+import { CheckoutItemContainer, ImageContainer, Name, 
+         Quantity, Price, RemoveButtonContainer } from './checkout-item.styles';
 
 const CheckoutItem = ({ cartItem, dispatch }) => {
   const { imageUrl, name, quantity, price } = cartItem
   return (
-    <div className='checkout-item'>
-      <div className='image-container'>
+    <CheckoutItemContainer>
+      <ImageContainer>
         <img src={imageUrl} alt='item' />
-      </div>
-      <span className='name'>{name}</span>
-      <span className='quantity'>
+      </ImageContainer>
+      <Name>{name}</Name>
+      <Quantity>
         <div className='arrow' onClick={() => 
           dispatch(removeItem(cartItem))}>&#10094;</div>
           <span className='value'>{quantity}</span>
         <div className='arrow' onClick={() => 
           dispatch(addItem(cartItem))}>&#10095;</div>
-      </span>
-      <span className='price'>{price}</span>
-      <div className='remove-button' onClick={() =>
-        dispatch(clearItemFromCart(cartItem))}>&#10005;</div>
-    </div>
+      </Quantity>
+      <Price>{price}</Price>
+      <RemoveButtonContainer onClick={() =>
+        dispatch(clearItemFromCart(cartItem))}>&#10005;
+      </RemoveButtonContainer>
+    </CheckoutItemContainer>
   )
 };
 
