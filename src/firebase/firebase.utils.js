906f03b9903b4a9926d344408cdaba3326c7ee88
Collins Muriuki <murerwacollins@gmail.com>
2020-02-24 00:04:11 +0300

    Minor refactoring on firebase.utils.js

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -20,10 +20,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   const snapshot = await userRef.get();
 
   if (!snapshot.exists) {
-    const {
-      displayName,
-      email
-    } = userAuth;
+    const { displayName, email } = userAuth;
     const createdAt = new Date();
 
     try {
