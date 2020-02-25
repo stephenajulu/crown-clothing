4bd578443810b8876234e1806a88598e28fbd375
Collins Muriuki <murerwacollins@gmail.com>
2020-02-25 12:43:04 +0300

    Revert app component to previous configuration

--- src/App.js
+++ src/App.js
@@ -11,10 +11,9 @@ import Header from './components/header/header.component';
 import AuthPage from './pages/auth/auth.component';
 import CheckOutPage from './pages/checkout/checkout.component';
 
-import { auth, createUserProfileDocument, addCollectionAndDocuments } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.action';
 import { selectCurrentUser } from './redux/user/user.selectors'
-import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
 
 
 class App extends React.Component {
@@ -22,7 +21,7 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser, collectionsArray } = this.props;
+    const { setCurrentUser } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -40,7 +39,6 @@ class App extends React.Component {
       // set to null if the current user is logged out
       setCurrentUser(userAuth);
 
-      addCollectionAndDocuments('collections', collectionsArray.map(({ title, items }) => ({ title, items })));
     });
   }
 
@@ -71,8 +69,7 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser,
-  collectionsArray: selectCollectionsForPreview
+  currentUser: selectCurrentUser
 });
 
 const matchDispatchToProps = dispatch => ({
