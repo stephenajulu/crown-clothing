f1201660ebd52ce2ac368422b9cb37385e0c0b41
Collins Muriuki <murerwacollins@gmail.com>
2020-02-25 16:37:37 +0300

    [redux] Adding shop data to redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { updateCollections } from '../../redux/shop/shop.actions'
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
@@ -10,10 +13,12 @@ class ShopPage extends React.Component {
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
+    const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
 
     collectionRef.onSnapshot(async snapshot => {
-      convertCollectionSnapshotToMap(snapshot);
+      const collectionsMap = convertCollectionSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
     });
   }
 
@@ -30,4 +35,9 @@ class ShopPage extends React.Component {
   }
 } 
 
-export default ShopPage;
+const mapDispatchToProps = dispatch => ({
+  updateCollections: collectionsMap => 
+    dispatch(updateCollections(collectionsMap))
+})
+
+export default connect(null, mapDispatchToProps)(ShopPage);
--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,11 +1,19 @@
 import SHOP_DATA from './shop.data';
 
+import { shopActionTypes } from './shop.types'
+
 const INITIAL_STATE = {
   shopItems: SHOP_DATA
 };
 
 const shopCollectionReducer = (state = INITIAL_STATE, action) => {
   switch(action.type) {
+    case shopActionTypes.UPDATE_COLLECTIONS:
+      return {
+        ...state,
+        collections: action.payload
+      }
+
     default:
       return state;
   }
