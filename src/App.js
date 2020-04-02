9c023b6c08b1f9bff90dd1e9a9b0a3d5482f1970
Collins Muriuki <murerwacollins@gmail.com>
2020-04-02 20:06:22 +0300

    Leverage useEffect hook in App component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect'
@@ -17,34 +17,30 @@ import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.action'
 
 
-class App extends React.Component {
-
-  componentDidMount() {
-    const { checkUserSession } = this.props;
+const App = ({ checkUserSession, currentUser }) => {
+  useEffect(() => {
     checkUserSession();
-  }
+  }, [checkUserSession]);
 
-  render() {
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route exact path='/checkout' component={CheckOutPage} />
-          <Route exact path='/signin' render={() =>
-            this.props.currentUser ? 
-            (
-              <Redirect to='/' />
-            ) : (
-              <AuthPage />
-            )
-          } />
-          <Route component={NoMatchPage} />
-        </Switch>
-      </div>
-    );
-  }  
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route exact path='/checkout' component={CheckOutPage} />
+        <Route exact path='/signin' render={() =>
+          currentUser ? 
+          (
+            <Redirect to='/' />
+          ) : (
+            <AuthPage />
+          )
+        } />
+        <Route component={NoMatchPage} />
+      </Switch>
+    </div>
+  );
 }
 
 const mapStateToProps = createStructuredSelector({
