fadf518d3df05608acfb59213e5d6874b0626e11
Collins Muriuki <murerwacollins@gmail.com>
2020-04-12 03:04:30 +0300

    Add get route to resolve service-worker

--- server.js
+++ server.js
@@ -29,6 +29,10 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
+app.get('/service-worker.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+});
+
 app.post('/payment', (req, res) => {
   const body = {
     source: req.body.token.id,
